68950bd708d961ff5603d96fcbada6fcf97e702b
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-07-11 17:01:09 +0300

    Create schemas

---
+++ sanity-backend/schemas/comment.js
@@ -0,0 +1,19 @@
+export default {
+    name: "comment",
+    title: "Comment",
+    type: "document",
+    fields: [
+      {
+        name: "postBy",
+        title: "Posted by",
+        type: "postedBy",
+      },
+      {
+          name:'comment',
+          title: 'Image',
+          type: 'Comment',
+  
+      }
+    ],
+  };
+  
